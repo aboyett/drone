4df4ad4456a30d91b92592a283de2ce283b4a657
Andy Boyett <andy@sisyphus.lan>
2017-09-06 18:27:37 -0700

    .drone.sh: also fetch broken deps

--- .drone.sh
+++ .drone.sh
@@ -3,5 +3,7 @@
 # only execute this script as part of the pipeline.
 [ -z "$CI" ] && echo "missing ci enviornment variable" && exit 2
 
+go get -d github.com/drone/drone-ui
+
 # build a static binary with the build number and extra features.
 go build -ldflags '-extldflags "-static" -X github.com/drone/drone/version.VersionDev=build.'${DRONE_BUILD_NUMBER} -o release/drone github.com/drone/drone/drone
